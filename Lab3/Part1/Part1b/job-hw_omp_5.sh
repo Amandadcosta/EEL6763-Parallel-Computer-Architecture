0075aff19419146eec8a2fde00983058f4373ba2
Amandadcosta <89613788+Amandadcosta@users.noreply.github.com>
2022-03-09 15:38:40 -0500

    Add files via upload

---
+++ Lab3/Part1/Part1b/job-hw_omp_5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=classdemo
+#SBATCH --account=eel6763
+#SBATCH --qos=eel6763
+#SBATCH --nodes=1  #(single node)
+#SBATCH --ntasks=1  #(single process)
+#SBATCH --cpus-per-task=8  #(32 class limit)
+#SBATCH --mem-per-cpu=600mb
+#SBATCH -t 00:05:00
+#SBATCH -o Part1_Fig1_8threads
+#SBATCH -e err
+export OMP_NUM_THREADS=8
+./a.out 1600 1600 1600
+
